a403781a7f644d74b004b8559106dd1e74cde73a
Bhaktij Koli <bhaktijkoli121@gmail.com>
2021-06-21 06:10:51 +0530

    chore: added commander

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
-
-console.log('Hello CLI')
+const path = require('path')
+const fs = require('fs')
+const { program } = require('commander')
+const packageJson = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf-8')
+)
+program.version(packageJson.version).description('Edhub CLI').parse()
